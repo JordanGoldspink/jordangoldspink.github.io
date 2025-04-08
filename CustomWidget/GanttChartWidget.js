ff7b3df77fc8a0846aef9d847254d2587530597e
JordanGoldspink <46961341+JordanGoldspink@users.noreply.github.com>
2025-04-08 15:24:40 +0100

    Add files via upload

--- CustomWidget/GanttChartWidget.js
+++ CustomWidget/GanttChartWidget.js
@@ -15,12 +15,11 @@ box-sizing: border-box;
 #image-container {
 width: 100%;
 height: 100px;
+
 background-position: center;
 }
 </style>
-<div id="image-container"> <svg width="750" height="100"> </svg></div>
-<div id="chart"></div>
-<a href="https://www.linkedin.com/company/planifyit" target="_blank" class="follow-link">Follow us on Linkedin - Planifyit</a>
+
 `;
 
         class GanttChartWidget extends HTMLElement {
